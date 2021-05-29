ced77a1925e0fe2da23e3aff00209e0b32f4627f
Sayak <sayak649@gmail.com>
2021-05-30 00:06:45 +0530

    new schema

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,13 @@
 const jwt = require("jsonwebtoken");
 
+async function protectedRoute(context, returnVal) {
+  const user = getUserId(context.req);
+  if (user !== "No token found" || user !== "Not authenticated") {
+    return await returnVal;
+  }
+  return user;
+}
+
 function getTokenPayload(token) {
   return jwt.verify(token, process.env.APP_SECRET);
 }
@@ -10,19 +18,28 @@ function getUserId(req, authToken) {
     if (authHeader) {
       const token = authHeader.replace("Bearer ", "");
       if (!token) {
-        throw new Error("No token found");
+        return "No token found";
       }
-      const { userId } = getTokenPayload(token);
-      return userId;
+      try {
+        const { userId } = getTokenPayload(token);
+        return userId;
+      } catch (err) {
+        return err.message;
+      }
     }
   } else if (authToken) {
-    const { userId } = getTokenPayload(authToken);
-    return userId;
+    try {
+      const { userId } = getTokenPayload(authToken);
+      return userId;
+    } catch (err) {
+      return err.message;
+    }
   }
 
-  throw new Error("Not authenticated");
+  return "Not authenticated";
 }
 
 module.exports = {
+  protectedRoute,
   getUserId,
 };
